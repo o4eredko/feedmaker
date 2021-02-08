f89da85ef5a5c6db49e2bb58792922c460cf39ad
burkovski <burkovski.danil@icloud.com>
2021-02-08 11:11:44 +0200

    Add router implementation to rest package

---
+++ infrastructure/rest/router.go
@@ -0,0 +1,28 @@
+package rest
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type (
+	Handler interface {
+		ListGenerations(w http.ResponseWriter, r *http.Request)
+		ListGenerationTypes(w http.ResponseWriter, r *http.Request)
+		GenerateFeed(w http.ResponseWriter, r *http.Request)
+		CancelGeneration(w http.ResponseWriter, r *http.Request)
+	}
+)
+
+func NewRouter(handler Handler) http.Handler {
+	router := mux.NewRouter()
+
+	generations := router.PathPrefix("/generations").Subrouter()
+	generations.HandleFunc("", handler.ListGenerations).Methods(http.MethodGet)
+	generations.HandleFunc("/types", handler.ListGenerationTypes).Methods(http.MethodGet)
+	generations.HandleFunc("/{generation-type}", handler.GenerateFeed).Methods(http.MethodPost)
+	generations.HandleFunc("/{generation-type}", handler.CancelGeneration).Methods(http.MethodDelete)
+
+	return router
+}
---
+++ infrastructure/rest/router_test.go
@@ -0,0 +1,73 @@
+package rest_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+
+	"go-feedmaker/infrastructure/rest"
+	"go-feedmaker/infrastructure/rest/mocks"
+)
+
+func TestNewRouter(t *testing.T) {
+	type args struct {
+		handler *mocks.Handler
+		request *http.Request
+	}
+	mustMakeArgs := func(method, path string) *args {
+		request, err := http.NewRequest(method, path, nil)
+		if err != nil {
+			panic(err)
+		}
+		return &args{
+			handler: new(mocks.Handler),
+			request: request,
+		}
+	}
+	testCases := []struct {
+		name       string
+		args       *args
+		setupMocks func(*args)
+	}{
+		{
+			name: "GET /generations",
+			args: mustMakeArgs(http.MethodGet, "/generations"),
+			setupMocks: func(args *args) {
+				args.handler.On("ListGenerations", mock.Anything, mock.Anything)
+			},
+		},
+		{
+			name: "GET /generations/types",
+			args: mustMakeArgs(http.MethodGet, "/generations/types"),
+			setupMocks: func(args *args) {
+				args.handler.On("ListGenerationTypes", mock.Anything, mock.Anything)
+			},
+		},
+		{
+			name: "POST /generations/foobar",
+			args: mustMakeArgs(http.MethodPost, "/generations/foobar"),
+			setupMocks: func(args *args) {
+				args.handler.On("GenerateFeed", mock.Anything, mock.Anything)
+			},
+		},
+		{
+			name: "DELETE /generations/foobar",
+			args: mustMakeArgs(http.MethodDelete, "/generations/foobar"),
+			setupMocks: func(args *args) {
+				args.handler.On("CancelGeneration", mock.Anything, mock.Anything)
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			testCase.setupMocks(testCase.args)
+			router := rest.NewRouter(testCase.args.handler)
+
+			router.ServeHTTP(httptest.NewRecorder(), testCase.args.request)
+
+			testCase.args.handler.AssertExpectations(t)
+		})
+	}
+}
