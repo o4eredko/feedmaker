7655f56001ec5a7bcf59b9b8f99b33120795c786
burkovski <burkovski.danil@icloud.com>
2021-02-11 13:36:52 +0200

    Add interface validation for cmd's args

--- infrastructure/scheduler/task/cmd.go
+++ infrastructure/scheduler/task/cmd.go
@@ -46,19 +46,58 @@ func makeArgs(arguments []interface{}) []reflect.Value {
 
 func validate(function reflect.Value, arguments []reflect.Value) error {
 	funcType := reflect.TypeOf(function.Interface())
+	if err := checkNumberOfArgs(funcType, arguments); err != nil {
+		return err
+	}
+	return checkTypesOfArgs(arguments, funcType)
+}
+
+func checkNumberOfArgs(funcType reflect.Type, arguments []reflect.Value) error {
 	numIn := funcType.NumIn()
 	numArgs := len(arguments)
 	if numIn != numArgs {
 		return fmt.Errorf("%w: expected %d arguments but got %d",
 			ErrArgumentsAmountMismatch, numIn, numArgs)
 	}
+	return nil
+}
+
+func checkTypesOfArgs(arguments []reflect.Value, funcType reflect.Type) error {
 	for i, arg := range arguments {
-		wantArgType := funcType.In(i).Kind()
-		gotArgType := arg.Kind()
-		if wantArgType != gotArgType {
-			return fmt.Errorf("%w: at position %d: expected %v but got %v",
-				ErrInvalidArgumentType, i, wantArgType, gotArgType)
+		wantArgType := funcType.In(i)
+		gotArgType := arg.Type()
+		if err := checkTypes(wantArgType, gotArgType); err != nil {
+			return fmt.Errorf("at position %d: %w", i, err)
 		}
+	}
+	return nil
+}
+
+func checkTypes(wantArgType, gotArgType reflect.Type) error {
+	var assertion func(reflect.Type, reflect.Type) error
+	switch wantArgType.Kind() {
+	case reflect.Interface:
+		assertion = assertAssignableToInterface
+	default:
+		assertion = assertSameType
+	}
+	return assertion(wantArgType, gotArgType)
+}
+
+func assertAssignableToInterface(wantArgType reflect.Type, gotArgType reflect.Type) error {
+	if !gotArgType.AssignableTo(wantArgType) {
+		return fmt.Errorf("%v is not asignable to %v: %w",
+			gotArgType, wantArgType, ErrInvalidArgumentType)
+	}
+	return nil
+}
+
+func assertSameType(wantArgType reflect.Type, gotArgType reflect.Type) error {
+	wantArgKind := wantArgType.Kind()
+	gotArgKind := gotArgType.Kind()
+	if wantArgKind != gotArgKind {
+		return fmt.Errorf("got %v, but expected %v: %w",
+			gotArgKind, wantArgKind, ErrInvalidArgumentType)
 	}
 	return nil
 }
